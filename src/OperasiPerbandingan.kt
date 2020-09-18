843bf569e7ceac8e60cdf2b3b924c45adad1f851
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-09-19 00:57:11 +0700

    kotlin

---
+++ src/OperasiPerbandingan.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val a = 100
+    val b = 200
+    val result: Boolean =a>b
+    println(result)
+////////////////////////////////////
+    val result1:Boolean = a<b
+    println(result1)
+}
